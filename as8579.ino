0d5d3d8a1a1a4435d02b30b00a9ff62ba490d4f3
Simon Martin <simon.martin@neurotrim-systems.de>
2022-03-21 22:04:01 +0100

    Update initialization function

--- as8579.ino
+++ as8579.ino
@@ -8,51 +8,46 @@
  */
 #include <SPI.h>
 
-// präprozessor 
-#define EDIVread 0x20 
-#define EDIVwrite 0x30
+const int chipSelectPin = 10;   
+uint16_t IQ[2] = {0, 0};  // array to store the I and Q values from the quick read function
 
-const int chipSelectPin = 10;
-uint16_t IQ[2] = {0, 0}; // array to store the I and Q values from the quick read function
 
 void setup() {
   pinMode(chipSelectPin, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200);
   SPI.begin();
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-  delay(10);
-  initialize();
   delay(100);
-  //Serial.println(read_two_bytes_from_register(0x27));
+  initialize();  
 }
 
+
 void loop() {
-  while (read_two_bytes_from_register(0x05) != 1){ // Wait for ADC complete Bit (Address: 0x05 / Value: 0x01)
-    Serial.println("Waitng for ADC to be ready");
+  while (read_two_bytes_from_register(0x05) != 1){ // Wait until ADC start is complete (Address: 0x05 / Value: 0x01)
+    Serial.println("Waiting for ADC to be ready");
     delay(100);
   }
   quick_read(IQ);
+  Serial.print("I: ");
   Serial.print(IQ[0]);
-  Serial.print("\t");
-  Serial.println(IQ[1]);
-  delay(100); 
+  Serial.print("\t Q: ");
+  Serial.println(IQ[1]); 
+  delay(50);
 }
-
-
 
 
 // ---------------------- WRITE FUNCTION ----------------------------------------------------
 
 void write_two_bytes_to_register(uint8_t address, uint8_t data1, uint8_t data2){
-  // this function writes the two bytes into the address register 
+  // this function writes two bytes into the address register 
   
-  digitalWrite(chipSelectPin, HIGH); // activates the sensor 
+  digitalWrite(chipSelectPin, HIGH);  // activate the sensor 
   
-  SPI.transfer(address);            // choose address
-  SPI.transfer(data1);       // send first byte
-  SPI.transfer(data2);       // send second byte
+  SPI.transfer(address);              // choose address
+  SPI.transfer(data1);                // send first byte
+  SPI.transfer(data2);                // send second byte
 
-  digitalWrite(chipSelectPin, LOW); // deactivates the sensor 
+  digitalWrite(chipSelectPin, LOW);   // deactivate the sensor 
 }
 
 // -------------------------------------------------------------------------------------------
@@ -60,22 +55,22 @@ void write_two_bytes_to_register(uint8_t address, uint8_t data1, uint8_t data2){
 // ------------------------ READ FUNCTION ----------------------------------------------------
 
 uint16_t read_two_bytes_from_register(uint8_t address){
-  // this function returns the two read bytes in one variable 
+  // this function returns the two read bytes as a uint16_t  
   
   uint8_t val1 = 0;
   uint8_t val2 = 0;
   uint16_t back = 0;
 
-  digitalWrite(chipSelectPin, HIGH); // activates the sensor 
+  digitalWrite(chipSelectPin, HIGH); // activate the sensor 
   
   SPI.transfer(address);            // choose address 
   val1 = SPI.transfer(0x00);        // catch returned byte
   val2 = SPI.transfer(0x00);        // catch returned byte 
 
-  digitalWrite(chipSelectPin, LOW); // deactivates the sensor 
+  digitalWrite(chipSelectPin, LOW); // deactivate the sensor 
 
-  back = val1 << 8;
-  back = back | val2;
+  back = val1 << 8;                 // shift by 8 bits to left
+  back = back | val2;               // OR operand 
   return back; 
 }
 
@@ -116,22 +111,34 @@ void quick_read(uint16_t IQ[]){
 // --------------------------------------------------------------------------------------------
 
 // --------------INITIALIZE FUNCTION ----------------------------------------------------------
-// This function should be called in the setup once to make the necessary settings. 
+// This function should be called in the setup once to make the necessary settings.
+// The "minimum settings", the "soft reset" and the "start sensing" have definitely to be made. 
+// (Compare datasheet: SPI Initialization)
+// With this initialization you shoud be able to read some I and Q values. 
+// The additional settings are then used to make the I and Q values useful.
+// Start experimenting with the PGA offset to get the I and Q values out of saturation. 
+//
+// (You can always read the status register (0x20) to see possible mistakes)
  
  void initialize() {
-  write_two_bytes_to_register(0x30, 0x00, 0x00);  // EDIV to 4MHz
-  write_two_bytes_to_register(0x31, 0x00, 0x01);  // Frequency to 125kHz
-  write_two_bytes_to_register(0x32, 0x00, 0x02);  // peak to peak to 1V
-  write_two_bytes_to_register(0x33, 0x00, 0x01);  // SEN0 chosen by MUX 
+  
+  // minimum settings 
+  write_two_bytes_to_register(0x30, 0x00, 0x00);  // EDIV: set system CLK   (here: 4MHz)
+  write_two_bytes_to_register(0x31, 0x00, 0x00);  // Set frequency          (here: 45.45kHz) 
+  write_two_bytes_to_register(0x32, 0x00, 0x02);  // Set peak to peak       (here: 1V)
+  write_two_bytes_to_register(0x33, 0x00, 0x01);  // Set MUX                (here: SEN0 line, output driver) 
   
   // additional settings 
-  //write_two_bytes_to_register(0x37, 0x00, 0x00);  // PGA gain control
-  //write_two_bytes_to_register(0x39, 0x00, 0x00);  // PGA offset control
-  //write_two_bytes_to_register(0x35, 0x00, 0x00);  // Current buffer gain
-  
+  //write_two_bytes_to_register(0x34, 0x00, 0x00);  // Set VAR_SEN
+  //write_two_bytes_to_register(0x35, 0x00, 0x00);  // Set Current Buffer Gain
+  write_two_bytes_to_register(0x37, 0x00, 0x00);  // Set PGA gain           (here: 36)
+  write_two_bytes_to_register(0x38, 0x00, 0x60);  // Set PGA I offset       (here: 0x60)
+  write_two_bytes_to_register(0x39, 0x00, 0x90);  // Set PGA Q offset       (here: 0x90)
   
   // soft reset 
   write_two_bytes_to_register(0x3A, 0x00, 0x28);  // reset DSP and start ADC
+
+  delay(100);
   
   // start sensing
   write_two_bytes_to_register(0x3A, 0x00, 0x2A);  // start continuous conversion  
